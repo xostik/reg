c039f7f3fa32238c9ec801dd360486c379ca0061
xostik <xostiki@ya.ru>
2015-11-15 15:43:27 +0500

    1

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+﻿module.exports = function (grunt) {
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+
+    grunt.initConfig({
+
+        less : {
+            default: {
+                src: ['less/main.less'],
+                dest: 'main.css'
+            }
+        }
+
+    });
+
+
+    grunt.task.registerTask('build',
+        [
+            'less'
+        ]
+    );
+};
